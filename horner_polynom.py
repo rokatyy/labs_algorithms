91a2843042eac29fc33dbd59b624d0c8511332d8
rokatyy <erogatova@gmail.com>
2020-09-13 21:53:03 +0300

    Made separate methods for each test

--- horner_polynom.py
+++ horner_polynom.py
@@ -3,7 +3,19 @@ from helper import generate_vector, make_plot
 
 
 @timeit
-def horner_polynom(poly, x=1):
-    f = [poly[0]]
-    for i in range(1, len(poly)):
-        f.append(poly[i - 1] * x + poly[i])
+def horner_polynom(vector, x=1):
+    p = vector[len(vector)-1]
+    for i in range(len(vector) - 2, 0, -1):
+        p = p * x + vector[i]
+
+
+if __name__ == "__main__":
+    max_value = 1900
+    k = max_value / 10
+    step = 1
+    x = [i for i in range(0, max_value, step)]
+    time = []
+    for vector_len in [i for i in range(1, max_value + 1, step)]:
+        vector = generate_vector(vector_len)
+        time.append(horner_polynom(vector, 1))
+    make_plot(x, time, 'Horner polynom', 5)
