98b2b826a9285ef999d1e97d34e8a16b40130423
rokatyy <erogatova@gmail.com>
2020-09-15 11:18:58 +0300

    first part done

--- lab1/timsort.py
+++ lab1/timsort.py
@@ -1,23 +1,83 @@
 from lab1.helper import generate_vector, make_plot, get_parameters, timeit
+import bisect
+
+
+def get_min_run(n):
+    """
+    We need to choose min_run so that N/min_run would be between 32 and 64
+    """
+    r = 0
+    while n >= 64:
+        r |= n & 1
+        n >>= 1
+    return n + r
+
+
+def insertionSort(array, left, right):
+    for i in range(left + 1, right + 1):
+        temp = array[i]
+        j = i - 1
+
+        while array[j] > temp and j >= left:
+            array[j + 1] = array[j]
+            j -= 1
+
+        array[j + 1] = temp
+
+
+def merge(array, left, middle, right):
+    """ Merges two sorted arrays """
+
+    len1 = middle - left + 1
+    len2 = right - middle
+
+    left_side = array[left: left + len1]
+    right_side = array[middle + 1: middle + len2 + 1]
+
+    i = j = 0
+    k = left
+
+    while i < len1 and j < len2:
+        if left_side[i] <= right_side[j]:
+            array[k] = left_side[i]
+            i += 1
+        else:
+            array[k] = right_side[j]
+            j += 1
+
+        k += 1
+
+    while i < len1:
+        array[k] = left_side[i]
+        i += 1
+        k += 1
+
+    while j < len2:
+        array[k] = right_side[j]
+        j += 1
+        k += 1
 
 
 @timeit
-def quicksort(array):
-    def quick_sort(array):
-        if len(array) < 2:
-            return array
-        pivot = array[-1]
-        left = []
-        right = []
-        for i in array[:-1]:
-            left.append(i) if i > pivot else right.append(i)
-        if len(left) > 1:
-            left = quick_sort(left)
-        if len(right) > 1:
-            right = quick_sort(right)
-        return right + [pivot] + left
+def timsort(array, length):
+    min_run = get_min_run(length)
+    for i in range(0, length, min_run):
+        insertionSort(array, i, min(i + 31, length - 1))
 
-    quick_sort(array)
+    size = min_run
+
+    while size < length:
+        left = 0
+
+        while left < length:
+            mid = min(left + size - 1, length - 1)
+            right = min(left + 2 * size - 1, length - 1)
+
+            merge(array, left, mid, right)
+
+            left += size * 2
+
+        size *= 2
 
 
 if __name__ == "__main__":
@@ -26,5 +86,5 @@ if __name__ == "__main__":
     time = []
     for vector_len in [i for i in range(1, max_value, step)]:
         vector = generate_vector(vector_len)
-        time.append(quicksort(vector))
+        time.append(timsort(vector, vector_len))
     make_plot(x, time, 'Timsort', 2)
