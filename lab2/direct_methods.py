706b3073964738fd94b0871e36e72685f7ed6358
rokatyy <erogatova@gmail.com>
2020-09-16 18:45:03 +0300

    dichotomy implemented

--- lab2/direct_methods.py
+++ lab2/direct_methods.py
@@ -1,14 +1,23 @@
-from math import ceil
+from math import ceil, fabs
 
 
 def exhaustive_search(a, b, e, func):
     n = ceil((b - a) / e)
-    minimum = min(func([a + k * (b - a) / n for k in range(n)]))
+    minimum = min([func(a + k * (b - a)) for k in range(n)])
     return minimum, n
 
 
-def dichotomy():
-    pass
+def dichotomy(a, b, e, func):
+    iterations = 0
+    s = e / 2
+    while fabs(a - b) >= e:
+        x1, x2 = (a + b - s) / 2, (a + b + s) / 2
+        if func(x1) <= func(x2):
+            a, b = a, x2
+        else:
+            a, b = x1, b
+        iterations += 1
+    return (a, b), iterations
 
 
 def golden_section():
--- lab2/main.py
+++ lab2/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import direct_methods
+from math import fabs
 import helper
 
 a = 0
@@ -7,6 +8,13 @@ b = 1
 e = 0.001
 
 if __name__ == "__main__":
-    y = (lambda x: [i ** 3 for i in x])
+
+    y = (lambda x: x**3)
     minimum, iterations = direct_methods.exhaustive_search(a, b, e, y)
-    print(f'Exaustive search in [{a},{b}] with e={e} found minimal value={minimum}, iteration count: {iterations}.')
+    print(f'Exaustive search for x^3 in [{a},{b}] with e={e} found minimal value={minimum}, iteration count: {iterations}.')
+
+    y = (lambda x: fabs(x-0.2))
+    minimum, iterations = direct_methods.dichotomy(a, b, e, y)
+    print(f'Dichotomy search for |x-0.2| in [{a},{b}] with e={e} found minimal value={minimum}, iteration count: {iterations}.')
+
+
