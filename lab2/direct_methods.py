07a3946832a94fad6ba06149ce1be9067cc288d5
rokatyy <erogatova@gmail.com>
2020-09-16 20:20:27 +0300

    golden section implemented

--- lab2/direct_methods.py
+++ lab2/direct_methods.py
@@ -1,4 +1,5 @@
 from math import ceil, fabs
+import numpy as np
 
 
 def exhaustive_search(a, b, e, func):
@@ -20,8 +21,24 @@ def dichotomy(a, b, e, func):
     return (a, b), iterations
 
 
-def golden_section():
-    pass
+def golden_section(a, b, e, func):
+    iterations = 0
+    gs = (3 - np.sqrt(5)) / 2
+    x1, x2 = a + gs * (b - a), b - gs * (b - a)
+    y1, y2 = func(x1), func(x2)
+    while fabs(a - b) >= e:
+        if y1 <= y2:
+            b, x2, y2 = x2, x1, y1
+            x1 = a + gs * (b - a)
+            y1 = func(x1)
+        else:
+            a, x1, y1 = x1, x2, y2
+            x2 = b - gs * (b - a)
+
+            y2 = func(x2)
+        iterations += 1
+
+    return (a, b), iterations
 
 
 def gauss():
--- lab2/main.py
+++ lab2/main.py
@@ -1,20 +1,28 @@
 import numpy as np
 import direct_methods
-from math import fabs
 import helper
 
 a = 0
 b = 1
 e = 0.001
 
-if __name__ == "__main__":
 
-    y = (lambda x: x**3)
+def one_dimensional(y, description, a, b, e):
     minimum, iterations = direct_methods.exhaustive_search(a, b, e, y)
-    print(f'Exaustive search for x^3 in [{a},{b}] with e={e} found minimal value={minimum}, iteration count: {iterations}.')
+    print(
+        f'Exaustive search for {description} in [{a},{b}] with e={e} found minimal value={minimum}, iteration count: {iterations}.')
 
-    y = (lambda x: fabs(x-0.2))
     minimum, iterations = direct_methods.dichotomy(a, b, e, y)
-    print(f'Dichotomy search for |x-0.2| in [{a},{b}] with e={e} found minimal value={minimum}, iteration count: {iterations}.')
+    print(
+        f'Dichotomy search for {description} in [{a},{b}] with e={e} found minimal value={minimum}, iteration count: {iterations}.')
+
+    minimum, iterations = direct_methods.golden_section(a, b, e, y)
+    print(
+        f'Golden section search for {description} in [{a},{b}] with e={e} found minimal value={minimum}, iteration count: {iterations}.')
 
 
+if __name__ == "__main__":
+    funcs = [((lambda x: abs(x - 0.2)), '|x-0.2|')]
+
+    for y in funcs:
+        one_dimensional(y[0], y[1], a, b, e)
